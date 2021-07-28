8d6ab0dd02c11a98dda52a1c383f5d179aa88080
Galo <galogamboa0@gmail.com>
2021-07-28 15:28:52 -0300

    finished homework

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,30 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var temp = [];
+  var i = 0;
+  while (i === 0){
+    temp = nombre[i].toUpperCase();
+    i++;
+    }
+  while (i < nombre.length){
+    temp = temp + nombre[i];
+    i++;
+  }
+  return temp
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +35,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var sumaIn = [0]
+  for (i=0; i < numeros.length; i++) {
+      sumaIn[0] = sumaIn[0] + numeros[i];
+    };
+  cb (sumaIn[0])
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  for (i = 0; i < array.length; i++){
+    cb(array[i]);
+    }
 }
 
 function map(array, cb) {
@@ -35,12 +56,24 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newArr = []
+  for (i = 0; i < array.length; i++){
+  newArr.push(cb(array[i]));
+  }
+  return newArr
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var newArr = []
+  for (i = 0; i < array.length; i++){
+  if (array[i][0] === 'a'){
+    newArr.push(array[i]);
+  }
+  }
+  return newArr;
 }
 
 // No modificar nada debajo de esta línea
